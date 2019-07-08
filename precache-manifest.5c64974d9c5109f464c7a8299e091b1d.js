58cf2af5e682a2797d13cb2f57df2693979d2c3a
ShawnYoung72443 <sryoung72443@gmail.com>
2019-07-08 02:32:49 -0400

    Updates

---
+++ precache-manifest.5c64974d9c5109f464c7a8299e091b1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea394fac3b3b03603c9f",
+    "url": "/React-todo-List/./static/js/runtime~main.ea394fac.js"
+  },
+  {
+    "revision": "66c59546eedc1307fa62",
+    "url": "/React-todo-List/./static/js/main.66c59546.chunk.js"
+  },
+  {
+    "revision": "4834d1d23bfd8820f7d7",
+    "url": "/React-todo-List/./static/js/2.4834d1d2.chunk.js"
+  },
+  {
+    "revision": "66c59546eedc1307fa62",
+    "url": "/React-todo-List/./static/css/main.df303b71.chunk.css"
+  },
+  {
+    "revision": "f09bc689970ed3c925f85daaf5cc36be",
+    "url": "/React-todo-List/./index.html"
+  }
+];
